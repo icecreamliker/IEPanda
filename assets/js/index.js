67905723336a8a92bcf21e05d9d0b69b48003b9d
姚立 <>
2012-06-14 00:20:31 +0800

    首页，注册等页面

---
+++ assets/js/index.js
@@ -0,0 +1,12 @@
+/*****************************************
+ * author: yaoli<yaoli111144@gmail.com>
+ * time:   2012-6-13
+ * desc:   js for index.html
+ ******************************************/
+
+$(document).ready(function(){
+  	$('.search-btn').click(function(ev){
+  		alert(1)
+
+  	});
+});
